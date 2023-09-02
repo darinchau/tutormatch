e4fedffcb26ab3c62092dfb3744786966d6b2afc
Darin Chau <darinchauyf@gmail.com>
2023-09-02 11:45:32 +0800

    Fix margins

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,5 +1,6 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
+#include <QDebug>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -7,8 +8,29 @@ MainWindow::MainWindow(QWidget *parent)
 {
     ui->setupUi(this);
 
-    // Set up all the buttons programmatically
+    // Do grid stuff
+    QGridLayout* const& grid_layout = this -> ui -> grid;
 
+    // Remove small gap between buttons
+    grid_layout -> setSpacing(0);
+    grid_layout->setContentsMargins(0, 0, 0, 0);
+
+    // Set up all the buttons programmatically
+    const int num_rows = 25;
+    const int num_cols = 5;
+    for (int row = 0; row < num_rows; ++row) {
+        for (int col = 0; col < num_cols; ++col) {
+            int button_number = row * num_cols + col;
+            QPushButton* button = new QPushButton("Button " + QString::number(button_number));
+            grid_layout ->addWidget(button, row, col);
+
+            // Optionally, connect a slot to handle button clicks
+            connect(button, &QPushButton::clicked, [=]() {
+                // Handle button click
+                qDebug() << "Button " << button_number << " clicked.";
+            });
+        }
+    }
 }
 
 MainWindow::~MainWindow()
